9dfe287cd4033835ef61c7a90fb3049b088deece
harry222333 <elahi.hamza99@gmail.com>
2025-02-11 15:26:55 +0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+ <div>
+  <h1 class="text-4xl font-bold underline">hello hamza</h1>
+ </div>
+  )
+}
+
+export default App
